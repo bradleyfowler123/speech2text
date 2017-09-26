5302dd6e706fe82110a98b903b0d916edded88a5
brad fowler <bradley.fowler@cubicatechnology.com>
2017-09-26 10:32:53 +0100

    backup

--- brad_model.py
+++ brad_model.py
@@ -25,7 +25,7 @@ tf.reset_default_graph()
 
 def io():		# The input to our model (encoder) is the pre-embedding sound vector. The input into the decoder is the time shifted word embeddings of the text
 	encoder_inputs_embedded = tf.placeholder(shape=(BATCH_SIZE, ENCODER_MAX_TIME, ENCODER_INPUT_DEPTH), dtype=tf.float32, name='encoder_inputs')  # [batch_size*length_of_sequence*20]
-	decoder_targets_indicies = tf.placeholder(shape=(BATCH_SIZE, DECODER_MAX_TIME), dtype=tf.int64, name='decoder_targets')	# [batch_size, max_time36]
+	decoder_targets_indicies = tf.placeholder(shape=(BATCH_SIZE, DECODER_MAX_TIME), dtype=tf.int64, name='decoder_targets_indicies')	# [batch_size, max_time36]
 	decoder_inputs_embedded = tf.placeholder(shape=(BATCH_SIZE, DECODER_MAX_TIME, DECODER_INPUT_DEPTH), dtype=tf.float32, name='decoder_inputs')
 	embed_normed = tf.constant(w2v.wordVectorsNormalised, dtype=tf.float32)
 
@@ -51,30 +51,39 @@ def inference(encoder_inputs_embedded, decoder_inputs_embedded):
 	return decoder_outputs, decoder_logits
 
 
-def loss(decoder_targets, decoder_logits, embed_normed):
+def loss(decoder_targets_inds, decoder_logits, embed_normed):
 
-	def cs(logits):
-		shifted_cosine = tf.matmul(logits, tf.transpose(embed_normed)) + 1	# 36*50 x 50*400002 = 36*400002
-		return -1* shifted_cosine**4
+	def cosineSimilarityArray(logits):																					# embed_normed is a list of the word vectors. logits is the output of the network (word vector prediction for every time point)
+		shifted_cosine = tf.matmul(logits, tf.transpose(embed_normed)) + 1												# DECODER_MAX_TIME*DECODER_INPUT_DEPTH x DECODER_INPUT_DEPTH*OUTPUT_VOCAB_SIZE = DECODER_MAX_TIME*OUTPUT_VOCAB_SIZE
+		return -1* shifted_cosine
+
+	"""
+	def getTargetCS(target_ind):
+		cs = []
+		for i in range(36):
+			cs.append(cosine_similarity[1][i][target_ind[0][i]])
+		return tf.stack(cs)
+	"""
+
 
 
 	with tf.name_scope('performance_metrics'):
-		# loss
-		decoder_logits_normalised = tf.nn.l2_normalize(decoder_logits, dim=2)
-		cosine_similarity = tf.map_fn(cs, decoder_logits_normalised)
-		#cosine_similarity = tf.losses.cosine_distance(embed_normed, normed_embedding, dim=2)
 
-		decoder_prediction = tf.argmax(cosine_similarity, 2)  # shape [batch_size, DECODER_MAX_TIME], type int64   # (BATCH_SIZE, DECODER_MAX_TIME)		- pick maximum liklihood words for each position (the index)
+		decoder_logits_normalised = tf.nn.l2_normalize(decoder_logits, dim=2)											# normalise the network output	(lookup cosine similarity formula if unsure why)
+		cosine_similarity = tf.map_fn(cosineSimilarityArray, decoder_logits_normalised)									# for the entire batch, compute the cosine similarity for every time point
 
+		decoder_prediction = tf.argmax(cosine_similarity, 2)   															# (BATCH_SIZE, DECODER_MAX_TIME)	- pick maximum liklihood words for each position (the word index)
 
-		# ------------ Tensorboard --------------- #
-		total_loss = tf.reduce_mean(cosine_similarity)
+		#total_loss = tf.reduce_max(cosine_similarity, 2)																# compute the lo
+		#total_loss = tf.map_fn(getTargetCS, (decoder_targets_inds, cosine_similarity))
+		# cosine_similarity = tf.losses.cosine_distance(decoder_logits, decoder_targets_inds, dim=1)
+	
 
 		tf.summary.scalar('summaries/total_loss', total_loss)
 
 
 		with tf.name_scope('correct_prediction'):
-			correct_prediction = tf.equal(decoder_prediction, decoder_targets)
+			correct_prediction = tf.equal(decoder_prediction, decoder_targets_inds)
 			with tf.name_scope('accuracy'):
 				accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 		tf.summary.scalar('accuracy', accuracy)  # log model output
@@ -84,6 +93,6 @@ def loss(decoder_targets, decoder_logits, embed_normed):
 
 def optimise(total_loss):
 	with tf.name_scope('train'):
-		train_step = tf.train.AdamOptimizer(1e-3).minimize(total_loss)
+		train_step = tf.train.AdamOptimizer(100e-3).minimize(total_loss)
 
 	return train_step
